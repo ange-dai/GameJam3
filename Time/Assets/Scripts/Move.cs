bc15d44faa3286d9c10ce04cbf3c2aaecfa5adab
Angela Dai <zhjh.angela@gmail.com>
2020-04-24 11:31:31 -0400

    added portals and different times

--- Time/Assets/Scripts/Move.cs
+++ Time/Assets/Scripts/Move.cs
@@ -8,7 +8,10 @@ public class Move : MonoBehaviour
     public float speed = 1.5f;
     private Vector3 spawnPoint= new Vector3(-1.05f,-4.67f,-1f);
     public bool gameOver;
+    public Camera cam;
     private Animator anim;
+    private float left=-10.55f;
+    private float right=10.51f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +24,12 @@ public class Move : MonoBehaviour
         //Debug.Log(gameOver);
         if(Input.anyKey){
           
-          if (Input.GetKey(KeyCode.LeftArrow)&&transform.position.x>-10.55) {
+          if (Input.GetKey(KeyCode.LeftArrow)&&transform.position.x>left) {
             transform.localRotation = Quaternion.Euler(0, 180, 0);
             anim.SetBool("WalkRights",true);
             transform.position += Vector3.left * speed * Time.deltaTime;
          }
-          if (Input.GetKey(KeyCode.RightArrow)&&transform.position.x<10.51) {
+          if (Input.GetKey(KeyCode.RightArrow)&&transform.position.x<right) {
             anim.SetBool("WalkRights",true);
             transform.localRotation = Quaternion.Euler(0, 0, 0);
             transform.position += Vector3.right * speed * Time.deltaTime;
@@ -43,10 +46,29 @@ public class Move : MonoBehaviour
             anim.SetBool("WalkRights",false);
             anim.SetBool("isJumping",false);
            
-        }
-    
-        
+        }    
     
     }
+  public void OnTriggerEnter2D(Collider2D other) {
+   if (other.gameObject.CompareTag("future")) {
+      transform.position=new Vector3(25.34f,-4f,-1f);
+      cam.transform.position=new Vector3(25.33f,0f,-10f);
+      right=32.42f;
+      left=17.32f;
+    }
+   if (other.gameObject.CompareTag("past")) {
+      transform.position=new Vector3(-24.8f,-4f,-1f);
+      cam.transform.position=new Vector3(-24.57f,0f,-10f);
+      right=-15.51f;
+      left=-32.96f;
+    }
+   if (other.gameObject.CompareTag("present")) {
+     transform.position=new Vector3(0f,-4f,-1f);
+     cam.transform.position=new Vector3(0,0f,-10f);
+     right=7.75f;
+     left=-6.41f;
+    }
+    
+  }
   
 }
