16ad9c37b343fbb14975f59b8dd519ec11c6db63
Angela Dai <zhjh.angela@gmail.com>
2020-05-05 11:34:37 -0400

    fixed portal and camera

--- Time/Assets/Scripts/Move.cs
+++ Time/Assets/Scripts/Move.cs
@@ -15,8 +15,8 @@ public class Move : MonoBehaviour
     {
       gameOver=false;
       anim=GetComponent<Animator>();
-      cam.orthographicSize = 5.750466f;
-      cam.transform.position=new Vector3(-0.46f,0.71f,-10f);
+      cam.orthographicSize = 6.27537f;
+      cam.transform.position=new Vector3(-0.6f,0.71f,-35.3f);
     }
     // Update is called once per frame
     void Update()
@@ -52,17 +52,17 @@ public class Move : MonoBehaviour
   public void OnTriggerEnter2D(Collider2D other) {
    if (other.gameObject.CompareTag("future")) {
       transform.position=new Vector3(25.45f,-4f,-1f);
-      cam.orthographicSize = 7.004026f;
-      cam.transform.position=new Vector3(25.33f,1.09f,-10f);
+      cam.orthographicSize = 7.605618f;
+      cam.transform.position=new Vector3(25.52f,0.71f,-35.3f);
     }
    if (other.gameObject.CompareTag("past")) {
       transform.position=new Vector3(-24.8f,-4f,-1f);
-      cam.transform.position=new Vector3(-25f,0.71f,-10f);
-      cam.orthographicSize = 6.087647f;
+      cam.transform.position=new Vector3(-25.01f,0.71f,-35.3f);
+      cam.orthographicSize = 6.59918f;
     }
    if (other.gameObject.CompareTag("present")) {
      transform.position=new Vector3(0f,-4f,-1f);
-     cam.orthographicSize = 5.750466f;
+     cam.orthographicSize = 6.27537f;
      cam.transform.position=new Vector3(-0.46f,0.71f,-10f);
     }
     
