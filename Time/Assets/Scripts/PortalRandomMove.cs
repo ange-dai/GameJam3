b8d394fe88d6c59e1d02ec86ce565c631f4cc28e
Angela Dai <zhjh.angela@gmail.com>
2020-05-12 17:58:21 -0400

    ending scene

--- Time/Assets/Scripts/PortalRandomMove.cs
+++ Time/Assets/Scripts/PortalRandomMove.cs
@@ -12,11 +12,13 @@ public class PortalRandomMove : MonoBehaviour
     private  float speed2;
     public int direction1; 
     public int direction2; 
+    private Vector3 scaleChange;
     // Start is called before the first frame update
     void Start()
     {
         speed1 = 10*Random.Range(0.0f, 1.0f);
         speed2= 10*Random.Range(0.0f, 1.0f);
+        scaleChange = new Vector3(0.002f, 0.002f, 0.002f);
     }
 
     // Update is called once per frame
@@ -24,26 +26,30 @@ public class PortalRandomMove : MonoBehaviour
     {
      checkDir();
      transform.Translate(speed1 * direction1 * Time.deltaTime, speed2 * direction2 * Time.deltaTime, 0);
-
+     transform.localScale += scaleChange;
         
     }
     public void checkDir(){
      if (transform.position.x >= endX){
       direction1=-1;
       speed1 = 10*Random.Range(0.0f, 1.0f);
+      scaleChange = -scaleChange;
       //speed2= rnd.NextDouble();
      }else if (transform.position.x <= startX){
       direction1=1;
       speed1 = 10*Random.Range(0.0f, 1.0f);
+      scaleChange = -scaleChange;
       //speed2= rnd.NextDouble();
      }else if (transform.position.y <= startY){
       direction2=1;
+      scaleChange = -scaleChange;
       //speed1 = rnd.NextDouble();
       speed2= 10*Random.Range(0.0f, 1.0f);
      }else if (transform.position.y >= endY){
-      direction2=1;
+      direction2=-1;
       //speed1 = rnd.NextDouble();
       speed2= 10*Random.Range(0.0f, 1.0f);
+      scaleChange = -scaleChange;
      }
     }
 }
---
+++ Time/Assets/Scripts/endingMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endingMove : MonoBehaviour
+{
+    public float speed;
+    private Animator anim;
+    public GameObject astro;
+    public float right;
+    public float left;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        cam.orthographicSize = 5f;
+        cam.transform.position = new Vector3(0f, 0f, -10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          if (Input.anyKey)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow)&&astro.transform.position.x>=left)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+                anim.SetBool("WalkRights", true);
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.RightArrow)&&astro.transform.position.x<=right)
+            {
+                anim.SetBool("WalkRights", true);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                anim.SetBool("WalkRights", false);
+                anim.SetBool("isJumping", true);
+                transform.Translate(Vector3.up * 7 * Time.deltaTime, Space.World);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += Vector3.down * speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            anim.SetBool("WalkRights", false);
+            anim.SetBool("isJumping", false);
+
+        }
+        
+    }
+}
